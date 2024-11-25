7285a53a374bde0878ae4ad8ef0df797a0ffcad6
Chitransh Shrivastava <94234674+chitranshsh@users.noreply.github.com>
2024-11-25 19:57:01 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+class config:
+    prefix = '-'
+    botID = '664c772d79bbb40ece258cdd'
+    botName = 'beta_tester_'
+    ownerID = '627cc7da7ddd872c3bec3f23'
+    ownerName = 'KingDraken'
+    roomName = 'HomeAlone'
+    roomOwner = 'KingDraken'
+
+class authorization:
+    room = "627cc7da7ddd872c3bec3f24"
+    token = "486423a78f41de2dda949776d43543489592205d0f8efe96d062947833c1bd16"
